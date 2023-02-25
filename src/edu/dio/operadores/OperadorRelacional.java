f1c4a149c2ca6c56d89f11fc2377c4dda39f09c1
guguiness <gus.nogueira@proton.me>
2023-02-25 13:03:04 -0300

    Adicionando Operador Relacional

---
+++ src/edu/dio/operadores/OperadorRelacional.java
@@ -0,0 +1,16 @@
+package edu.dio.operadores;
+
+public class OperadorRelacional {
+    public static void main(String[] args) {
+        String nome1 = "José";
+        String nome2 = "José";
+        System.out.print("Objetos iguais, conteúdos iguais: ");
+        System.out.println(nome1 == nome2);
+
+        String nome3 = "Maria";
+        String nome4 = new String("Maria");
+        System.out.println("Objetos diferentes, conteúdos iguais: ");
+        System.out.println("operador ==  -> " + (nome3 == nome4));
+        System.out.println(".equals() -> " + nome3.equals(nome4));
+    }
+}
