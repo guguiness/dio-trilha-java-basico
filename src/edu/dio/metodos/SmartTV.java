8fdca3890f5ac41e5a5661b6f08400da64efe921
guguiness <gus.nogueira@proton.me>
2023-02-26 12:36:19 -0300

    Adicionando/consumindo métodos da classe SmartTV

--- src/edu/dio/metodos/SmartTV.java
+++ src/edu/dio/metodos/SmartTV.java
@@ -4,4 +4,34 @@ public class SmartTV {
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
+
+    public void ligar() {
+        this.ligada = true;
+    }
+
+    public void desligar() {
+        this.ligada = false;
+    }
+
+    public void mudarCanal(int novoCanal) {
+        this.canal = novoCanal;
+    }
+
+    public void aumentarCanal() {
+        this.canal++;
+    }
+
+    public void diminuirCanal() {
+        this.canal--;
+    }
+
+    public void aumentarVolume() {
+        this.volume++;
+        System.out.println("Aumentando o volume para: " + volume);
+    }
+
+    public void diminuirVolume() {
+        this.volume--;
+        System.out.println("Diminuindo o volume para: " + volume);
+    }
 }
--- src/edu/dio/metodos/Usuario.java
+++ src/edu/dio/metodos/Usuario.java
@@ -4,8 +4,22 @@ public class Usuario {
     public static void main(String[] args) {
         SmartTV smartTV = new SmartTV();
 
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.aumentarVolume();
+
+        System.out.println("Canal atual: " + smartTV.canal);
+        smartTV.mudarCanal(13);
+        System.out.println("Canal atual: " + smartTV.canal);
+
+        System.out.println("Volume: " + smartTV.volume);
+
         System.out.println("TV ligada? " + smartTV.ligada);
         System.out.println("Canal atual: " + smartTV.canal);
         System.out.println("Volume: " + smartTV.volume);
+
+        smartTV.ligar();
+        System.out.println("Novo Status -> TV ligada? " + smartTV.ligada);
     }
 }
