1f19c8734a1c01392843e604aae52045a5d31f4b
guguiness <gus.nogueira@proton.me>
2023-02-26 12:07:52 -0300

    Criando arquivos da aula de Métodos

---
+++ src/edu/dio/metodos/App.java
@@ -0,0 +1,7 @@
+package edu.dio.metodos;
+
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/edu/dio/metodos/SmartTV.java
@@ -0,0 +1,5 @@
+package edu.dio.metodos;
+
+public class SmartTV {
+    
+}
