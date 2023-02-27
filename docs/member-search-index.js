8aa49138ffde512afa9a8d267f36265f1427297e
guguiness <gus.nogueira@proton.me>
2023-02-27 16:57:26 -0300

    Adição de documentação gerada por JavaDocs

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"edu.dio.documentacao","c":"Calculadora","l":"Calculadora()","u":"%3Cinit%3E()"},{"p":"edu.dio.documentacao","c":"Calculadora","l":"somar(int, int)","u":"somar(int,int)"}];updateSearchResults();
