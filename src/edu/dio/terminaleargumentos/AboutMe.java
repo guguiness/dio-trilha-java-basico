1ca43a1ce3fc1b16cf31a66b57fabbb6cee3eeb6
guguiness <gus.nogueira@proton.me>
2023-02-28 15:21:17 -0300

    Alterando AboutMe para usar a classe Scanner

--- src/edu/dio/terminaleargumentos/AboutMe.java
+++ src/edu/dio/terminaleargumentos/AboutMe.java
@@ -1,11 +1,23 @@
 package edu.dio.terminaleargumentos;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.print("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.print("Digite sua altura: ");
+        double altura = scanner.nextDouble();
 
         System.out.println("Olá, meu nome é " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos");
