e452d3a95c61d353f591561e5058675e2acbd34f
guguiness <gus.nogueira@proton.me>
2023-03-15 15:25:02 -0300

    Refatorando classe No com generics

--- src/edu/dio/estruturadedados/projetono/Main.java
+++ src/edu/dio/estruturadedados/projetono/Main.java
@@ -2,29 +2,21 @@ package edu.dio.estruturadedados.projetono;
 
 public class Main {
     public static void main(String[] args) {
-        No not1 = new No("Conteúdo Nó 1");
-        System.out.println(not1);
+        No<String> no1 = new No<>("Conteúdo Nó 1");
 
-        No not2 = new No("Conteúdo Nó 2");
-        System.out.println(not2);
-
-        System.out.println("--------------------");
-
-        No no1 = new No("Conteúdo Nó 1");
-        System.out.println(no1);
-
-        No no2 = new No("Conteúdo Nó 2");
+        No<String> no2 = new No<>("Conteúdo Nó 2");
         no1.setProximoNo(no2);
-        System.out.println(no1.getProximoNo());
 
-        No no3 = new No("Conteúdo Nó 3");
+        No<String> no3 = new No<>("Conteúdo Nó 3");
         no2.setProximoNo(no3);
-        System.out.println(no1.getProximoNo().getProximoNo());
 
-        No no4 = new No("Conteúdo Nó 4");
+        No<String> no4 = new No<>("Conteúdo Nó 4");
         no3.setProximoNo(no4);
-        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
         
+        System.out.println(no1);
+        System.out.println(no1.getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
         System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
     }
 }
