5f4282b2cec327e32295151ed47a6259d29c241c
guguiness <gus.nogueira@proton.me>
2023-03-28 15:46:58 -0300

    Adicionando getter e setter para noProximo e noPrevio em NoDuplo

--- src/edu/dio/estruturadedados/listaduplamenteencadeada/NoDuplo.java
+++ src/edu/dio/estruturadedados/listaduplamenteencadeada/NoDuplo.java
@@ -17,6 +17,22 @@ public class NoDuplo<T> {
 		this.conteudo = conteudo;
 	}
 
+	public NoDuplo<T> getNoProximo() {
+		return noProximo;
+	}
+
+	public void setNoProximo(NoDuplo<T> noProximo) {
+		this.noProximo = noProximo;
+	}
+
+	public NoDuplo<T> getNoPrevio() {
+		return noPrevio;
+	}
+
+	public void setNoPrevio(NoDuplo<T> noPrevio) {
+		this.noPrevio = noPrevio;
+	}
+
 	@Override
 	public String toString() {
 		return "NoDuplo [conteudo=" + conteudo + "]";
