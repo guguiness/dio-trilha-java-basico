1c35d8c57b2bd99aa02c65269364dc78ad79214e
guguiness <gus.nogueira@proton.me>
2023-04-05 16:55:42 -0300

    Adicionando método inserir() na árvore binária

---
+++ src/edu/dio/estruturadedados/arvorebinaria/ArvoreBinaria.java
@@ -0,0 +1,34 @@
+package edu.dio.estruturadedados.arvorebinaria;
+
+public class ArvoreBinaria<T extends Comparable<T>> {
+	private BinNo<T> raiz;
+	
+	public ArvoreBinaria() {
+		this.setRaiz(null);
+	}
+	
+	public void inserir(T conteudo) {
+		BinNo<T> novoNo = new BinNo<>(conteudo);
+		raiz = inserir(raiz, novoNo);
+	}
+	
+	private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo) {
+		if(atual == null) {
+			return novoNo;
+		} else if(novoNo.getConteudo().compareTo(atual.getConteudo()) < 0) {
+			atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
+		} else {
+			atual.setNoDir(inserir(atual.getNoDir(), novoNo));
+		}
+		
+		return atual;
+	}
+
+	public BinNo<T> getRaiz() {
+		return raiz;
+	}
+
+	public void setRaiz(BinNo<T> raiz) {
+		this.raiz = raiz;
+	}
+}
