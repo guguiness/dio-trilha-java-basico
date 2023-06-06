d428f1615cefd641b15ba743a538801fbd3dd27f
guguiness <gus.nogueira@proton.me>
2023-06-06 15:48:06 -0300

    Adicionando classes Bootcamp e Dev

---
+++ desafios/bootcamp/src/Bootcamp.java
@@ -0,0 +1,82 @@
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp {
+    private String nome;
+    private String descricao;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private Set<Dev> devsInscritos;
+    private Set<Atividade> atividades;
+
+    public Bootcamp() {
+        this.dataInicio = LocalDate.now();
+        this.dataFim = this.dataInicio.plusDays(45);
+        this.devsInscritos = new HashSet<>();
+        this.atividades = new LinkedHashSet<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Set<Dev> getDevsInscritos() {
+        return devsInscritos;
+    }
+
+    public void setDevsInscritos(Set<Dev> devsInscritos) {
+        this.devsInscritos = devsInscritos;
+    }
+
+    public Set<Atividade> getAtividades() {
+        return atividades;
+    }
+
+    public void setAtividades(Set<Atividade> atividades) {
+        this.atividades = atividades;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(nome, bootcamp.nome) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(dataInicio, bootcamp.dataInicio) && Objects.equals(dataFim, bootcamp.dataFim) && Objects.equals(devsInscritos, bootcamp.devsInscritos) && Objects.equals(atividades, bootcamp.atividades);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, descricao, dataInicio, dataFim, devsInscritos, atividades);
+    }
+}
